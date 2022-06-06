2d3687aaf01976c15ee85078be315e2e54a4e1f7
UBC Seagull <ubcseagull@gmail.com>
2022-06-05 20:58:41 -0700

    Clean up decensor mildly

--- Py/decensor.py
+++ Py/decensor.py
@@ -1,11 +1,7 @@
+import config
+import os
+import shutil
 
-try:
-    # default library
-    import os, logging, sys, config, shutil
-except ImportError as e:
-    print("Error when importing DEFAULT library : ", e)
-    print("\nIf you made script named [\"os.py\", \"logging.py\", \"sys.py\", \"config.py\"] rename it")
-    print("If not, re-install python or check your Python environment variables")
 try:
     # local library
     import file
@@ -15,12 +11,13 @@ try:
     import numpy as np
     from PIL import Image
     import tensorflow as tf
-    from PySide2 import QtCore # for QThread
+    from PySide2 import QtCore  # for QThread
 except ImportError as e:
-    print("\n"+ '='*20 + " ImportError " + "=" * 20 + "\n")
+    print("\n" + '=' * 20 + " ImportError " + "=" * 20 + "\n")
     if e.__class__.__name__ == "ModuleNotFoundError":
         print(e)
-        print("Python libraries are missing. You can install all required libraries by running in the command line (terminal)")
+        print(
+            "Python libraries are missing. You can install all required libraries by running in the command line (terminal)")
         print("cpu version : pip install -r requirements-cpu.txt")
         print("gpu version : pip install -r requirements-gpu.txt")
     else:
@@ -29,15 +26,16 @@ except ImportError as e:
     print("install Anaconda : https://www.anaconda.com/distribution/ \n")
 
 os.environ["TF_XLA_FLAGS"] = "--tf_xla_cpu_global_jit"
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' # show XLA warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'  # show XLA warnings
+
 
 class Decensor(QtCore.QThread):
-    def __init__(self, parentThread = None, text_edit = None, text_cursor = None, ui_mode = None):
+    def __init__(self, parentThread=None, text_edit=None, text_cursor=None, ui_mode=None):
         super().__init__(parentThread)
         args = config.get_args()
         self.is_mosaic = args.is_mosaic
         self.variations = args.variations
-        self.mask_color = [args.mask_color_red/255.0, args.mask_color_green/255.0, args.mask_color_blue/255.0]
+        self.mask_color = [args.mask_color_red / 255.0, args.mask_color_green / 255.0, args.mask_color_blue / 255.0]
         self.decensor_input_path = args.decensor_input_path
         self.decensor_input_original_path = args.decensor_input_original_path
         self.decensor_output_path = args.decensor_output_path
@@ -45,21 +43,11 @@ class Decensor(QtCore.QThread):
         self.model = None
         self.warm_up = False
 
-        # if ui_mode is not None:
-        #     self.ui_mode = ui_mode
-        # else:
-        #     self.ui_mode = args.ui_mode
-        #
-        # if self.ui_mode:
-        #     self.text_edit = text_edit
-        #     self.text_cursor = text_cursor
-        #     self.ui_mode = True
-
         if not os.path.exists(self.decensor_output_path):
             os.makedirs(self.decensor_output_path)
 
     def run(self):
-        if not self.warm_up :
+        if not self.warm_up:
             print("if self.warm_up :")
             self.load_model()
             return
@@ -85,18 +73,18 @@ class Decensor(QtCore.QThread):
             bar_checkpoint = os.path.join(cwd, "Py/models/bar/")
             mosaic_model = os.path.join(cwd, "Py/models/mosaic/Train_290000.meta")
             mosaic_checkpoint = os.path.join(cwd, "Py/models/mosaic/")
-            self.model = InpaintNN(bar_model_name = bar_model,
-                                   bar_checkpoint_name = bar_checkpoint,
-                                   mosaic_model_name = mosaic_model,
-                                   mosaic_checkpoint_name = mosaic_checkpoint,
+            self.model = InpaintNN(bar_model_name=bar_model,
+                                   bar_checkpoint_name=bar_checkpoint,
+                                   mosaic_model_name=mosaic_model,
+                                   mosaic_checkpoint_name=mosaic_checkpoint,
                                    is_mosaic=self.is_mosaic)
         self.warm_up = True
         print("load model finished")
         print("Loading model finished!\n")
 
     def decensor_all_images_in_folder(self):
-        #load model once at beginning and reuse same model
-        if not self.warm_up :
+        # load model once at beginning and reuse same model
+        if not self.warm_up:
             # incase of running by source code
             self.load_model()
 
@@ -115,54 +103,46 @@ class Decensor(QtCore.QThread):
         '''
         print("Decensoring {} image files\n".format(len(file_names)))
 
-        #convert all images into np arrays and put them in a list
-        for n, file_name in enumerate(file_names, start = 1):
-            '''
-            self.update_ProgressBar_SET_VALUE.emit(n)
-            print("Decensoring {} / {}".format(n, len(file_names)))
-            '''
+        # convert all images into np arrays and put them in a list
+        for n, file_name in enumerate(file_names, start=1):
             print("Decensoring image file : {}\n".format(file_name))
-
-            # signal progress bar value == masks decensored on image ,
-            # e.g) sample image : 17
 
             color_file_path = os.path.join(input_color_dir, file_name)
             color_basename, color_ext = os.path.splitext(file_name)
             if os.path.isfile(color_file_path) and color_ext.casefold() == ".png":
                 print("--------------------------------------------------------------------------")
                 print("Decensoring the image {}\n".format(color_file_path))
-                try :
+                try:
                     colored_img = Image.open(color_file_path)
                 except:
-                    print("Cannot identify image file (" +str(color_file_path)+")")
-                    self.files_removed.append((color_file_path,3))
+                    print("Cannot identify image file (" + str(color_file_path) + ")")
+                    self.files_removed.append((color_file_path, 3))
                     # incase of abnormal file format change (ex : text.txt -> text.png)
                     continue
 
-                #if we are doing a mosaic decensor
+                # if we are doing a mosaic decensor
                 if self.is_mosaic:
-                    #get the original file that hasn't been colored
+                    # get the original file that hasn't been colored
                     ori_dir = self.decensor_input_original_path
                     test_file_names = os.listdir(ori_dir)
-                    #since the original image might not be a png, test multiple file formats
+                    # since the original image might not be a png, test multiple file formats
                     valid_formats = {".png", ".jpg", ".jpeg"}
                     for test_file_name in test_file_names:
                         test_basename, test_ext = os.path.splitext(test_file_name)
                         if (test_basename == color_basename) and (test_ext.casefold() in valid_formats):
                             ori_file_path = os.path.join(ori_dir, test_file_name)
                             ori_img = Image.open(ori_file_path)
-                            # colored_img.show()
                             self.decensor_image_variations(ori_img, colored_img, file_name)
                             break
-                    else: #for...else, i.e if the loop finished without encountering break
+                    else:  # for...else, i.e if the loop finished without encountering break
                         print("Corresponding original, uncolored image not found in {}".format(color_file_path))
                         print("Check if it exists and is in the PNG or JPG format.")
-                #if we are doing a bar decensor
+                # if we are doing a bar decensor
                 else:
                     self.decensor_image_variations(colored_img, colored_img, file_name)
             else:
                 print("--------------------------------------------------------------------------")
-                print("Image can't be found: "+str(color_file_path))
+                print("Image can't be found: " + str(color_file_path))
 
         print("--------------------------------------------------------------------------")
         if self.files_removed is not None:
@@ -176,7 +156,7 @@ class Decensor(QtCore.QThread):
                 shutil.copy(old_path, new_path)
         print("\nDecensoring complete!")
 
-        #unload model to prevent memory issues
+        # unload model to prevent memory issues
         print("\nUnloading model")
         tf.reset_default_graph()
 
@@ -184,7 +164,7 @@ class Decensor(QtCore.QThread):
         for i in range(self.variations):
             self.decensor_image_variation(ori, colored, i, file_name)
 
-    #create different decensors of the same image by flipping the input image
+    # create different decensors of the same image by flipping the input image
     def apply_variant(self, image, variant_number):
         if variant_number == 0:
             return image
@@ -195,41 +175,35 @@ class Decensor(QtCore.QThread):
         else:
             return image.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.FLIP_TOP_BOTTOM)
 
-    #decensors one image at a time
-    #TODO: decensor all cropped parts of the same image in a batch (then i need input for colored an array of those images and make additional changes)
+    # decensors one image at a time
+    # TODO: decensor all cropped parts of the same image in a batch (then i need input for colored an array of those images and make additional changes)
     def decensor_image_variation(self, ori, colored, variant_number, file_name):
         ori = self.apply_variant(ori, variant_number)
         colored = self.apply_variant(colored, variant_number)
-        width, height = ori.size
-        #save the alpha channel if the image has an alpha channel
+        # save the alpha channel if the image has an alpha channel
         has_alpha = False
         if (ori.mode == "RGBA"):
             has_alpha = True
-            alpha_channel = np.asarray(ori)[:,:,3]
-            alpha_channel = np.expand_dims(alpha_channel, axis =-1)
+            alpha_channel = np.asarray(ori)[:, :, 3]
+            alpha_channel = np.expand_dims(alpha_channel, axis=-1)
             ori = ori.convert('RGB')
 
         ori_array = image_to_array(ori)
-        ori_array = np.expand_dims(ori_array, axis = 0)
+        ori_array = np.expand_dims(ori_array, axis=0)
 
         if self.is_mosaic:
-            #if mosaic decensor, mask is empty
-            # mask = np.ones(ori_array.shape, np.uint8)
-            # print(mask.shape)
+
             colored = colored.convert('RGB')
             color_array = image_to_array(colored)
-            color_array = np.expand_dims(color_array, axis = 0)
+            color_array = np.expand_dims(color_array, axis=0)
             mask = self.find_mask(color_array)
-            mask_reshaped = mask[0,:,:,:] * 255.0
-            mask_img = Image.fromarray(mask_reshaped.astype('uint8'))
-            # mask_img.show()
 
         else:
             mask = self.find_mask(ori_array)
 
-        #colored image is only used for finding the regions
-        regions = find_regions(colored.convert('RGB'), [v*255 for v in self.mask_color])
-        print("Found {region_count} censored regions in this image!".format(region_count = len(regions)))
+        # colored image is only used for finding the regions
+        regions = find_regions(colored.convert('RGB'), [v * 255 for v in self.mask_color])
+        print("Found {region_count} censored regions in this image!".format(region_count=len(regions)))
 
         if len(regions) == 0 and not self.is_mosaic:
             print("No green (0,255,0) regions detected! Make sure you're using exactly the right color.")
@@ -237,81 +211,50 @@ class Decensor(QtCore.QThread):
 
         print("Found {} masked regions".format(len(regions)))
 
-
         output_img_array = ori_array[0].copy()
 
         for region_counter, region in enumerate(regions, 1):
-            print("Decensoring regions in image, Decensoring censor {}/{}".format(region_counter,len(regions)))
+            print("Decensoring regions in image, Decensoring censor {}/{}".format(region_counter, len(regions)))
             bounding_box = expand_bounding(ori, region, expand_factor=1.5)
             crop_img = ori.crop(bounding_box)
-            # crop_img.show()
-            #convert mask back to image
-            mask_reshaped = mask[0,:,:,:] * 255.0
+            # convert mask back to image
+            mask_reshaped = mask[0, :, :, :] * 255.0
             mask_img = Image.fromarray(mask_reshaped.astype('uint8'))
-            #resize the cropped images
+            # resize the cropped images
             crop_img = crop_img.resize((256, 256))
             crop_img_array = image_to_array(crop_img)
-            #resize the mask images
+            # resize the mask images
             mask_img = mask_img.crop(bounding_box)
             mask_img = mask_img.resize((256, 256))
             # mask_img.show()
-            #convert mask_img back to array
+            # convert mask_img back to array
             mask_array = image_to_array(mask_img)
-            #the mask has been upscaled so there will be values not equal to 0 or 1
-
-            # mask_array[mask_array > 0] = 1
-            # crop_img_array[..., :-1][mask_array==0] = (0,0,0)
+            # the mask has been upscaled so there will be values not equal to 0 or 1
 
             if not self.is_mosaic:
-                a, b = np.where(np.all(mask_array == 0, axis = -1))
-                # print(a,b)
-                # print(crop_img_array[a,b])
-                # print(crop_img_array[a,b,0])
-                # print(crop_img_array.shape)
-                # print(type(crop_img_array[0,0]))
-                crop_img_array[a,b,:] = 0.
-            # temp = Image.fromarray((crop_img_array * 255.0).astype('uint8'))
-            # temp.show()
+                a, b = np.where(np.all(mask_array == 0, axis=-1))
+                crop_img_array[a, b, :] = 0.
 
-            crop_img_array = np.expand_dims(crop_img_array, axis = 0)
-            mask_array = np.expand_dims(mask_array, axis = 0)
-
-            # print(np.amax(crop_img_array))
-            # print(np.amax(mask_array))
-            # print(np.amax(masked))
-
-            # print(np.amin(crop_img_array))
-            # print(np.amin(mask_array))
-            # print(np.amin(masked))
-
-            # print(mask_array)
+            crop_img_array = np.expand_dims(crop_img_array, axis=0)
+            mask_array = np.expand_dims(mask_array, axis=0)
 
             crop_img_array = crop_img_array * 2.0 - 1
-            # mask_array = mask_array / 255.0
 
             # Run predictions for this batch of images
             pred_img_array = self.model.predict(crop_img_array, crop_img_array, mask_array)
 
-            pred_img_array = np.squeeze(pred_img_array, axis = 0)
+            pred_img_array = np.squeeze(pred_img_array, axis=0)
             pred_img_array = (255.0 * ((pred_img_array + 1.0) / 2.0)).astype(np.uint8)
 
-            #scale prediction image back to original size
-            bounding_width = bounding_box[2]-bounding_box[0]
-            bounding_height = bounding_box[3]-bounding_box[1]
-            #convert np array to image
-
-            # print(bounding_width,bounding_height)
-            # print(pred_img_array.shape)
+            bounding_width = bounding_box[2] - bounding_box[0]
+            bounding_height = bounding_box[3] - bounding_box[1]
 
             pred_img = Image.fromarray(pred_img_array.astype('uint8'))
-            # pred_img.show()
-            pred_img = pred_img.resize((bounding_width, bounding_height), resample = Image.BICUBIC)
-            # pred_img.show()
+            pred_img = pred_img.resize((bounding_width, bounding_height), resample=Image.BICUBIC)
 
             pred_img_array = image_to_array(pred_img)
 
-            # print(pred_img_array.shape)
-            pred_img_array = np.expand_dims(pred_img_array, axis = 0)
+            pred_img_array = np.expand_dims(pred_img_array, axis=0)
 
             # copy the decensored regions into the output image
             for i in range(len(ori_array)):
@@ -320,14 +263,16 @@ class Decensor(QtCore.QThread):
                         bounding_width_index = col + bounding_box[0]
                         bounding_height_index = row + bounding_box[1]
                         if (bounding_width_index, bounding_height_index) in region:
-                            output_img_array[bounding_height_index][bounding_width_index] = pred_img_array[i,:,:,:][row][col]
-            print("{region_counter} out of {region_count} regions decensored.".format(region_counter=region_counter, region_count=len(regions)))
+                            output_img_array[bounding_height_index][bounding_width_index] = \
+                            pred_img_array[i, :, :, :][row][col]
+            print("{region_counter} out of {region_count} regions decensored.".format(region_counter=region_counter,
+                                                                                      region_count=len(regions)))
 
         output_img_array = output_img_array * 255.0
 
-        #restore the alpha channel if the image had one
+        # restore the alpha channel if the image had one
         if has_alpha:
-            output_img_array = np.concatenate((output_img_array, alpha_channel), axis = 2)
+            output_img_array = np.concatenate((output_img_array, alpha_channel), axis=2)
 
         output_img = Image.fromarray(output_img_array.astype('uint8'))
         output_img = self.apply_variant(output_img, variant_number)
@@ -335,7 +280,7 @@ class Decensor(QtCore.QThread):
         print("current image finished")
 
         if file_name != None:
-            #save the decensored image
+            # save the decensored image
             base_name, ext = os.path.splitext(file_name)
             file_name = base_name + ext
             save_path = os.path.join(self.decensor_output_path, file_name)
